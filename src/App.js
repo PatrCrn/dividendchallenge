65cb4a031ced41e6f66ea8708f4bf44a82589459
PatrCrn <patrick.corneo@etu.hesge.ch>
2022-01-31 13:41:19 +0100

    pepePush

--- src/App.js
+++ src/App.js
@@ -1,22 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+    const [companies, setCompanies] = useState([]);
+    //const [companies, setCompanies] = useState([]);
+
+    useEffect(async () => {
+        await getCompanies()
+            .then(displayCompanies);
+    }, []);
+
+    const getCompanies = async _ => {
+        await fetch("https://gist.githubusercontent.com/VincentLeV/a0c326b9cbeabf63b4e5e02aa9779f6c/raw/b916a9e3d40aef926bf7e3b9b4db308d7da1ca5d/shares.json")
+            .then(response => response.json())
+            .then(responseJson => setCompanies(responseJson))
+            .then(_ => console.log(companies))
+    }
+
+    const displayCompanies = _=>{
+        for (let i = 0; i < companies.length; i++) {
+            let divHistory = companies[i].dividendHistory;
+            let price = companies[i].price;
+
+            // CALCULATING LAST YIELD %
+            let yiield;
+            yiield = (divHistory[0].dividend / price) * 100;
+
+
+            // CALCULATING AVERAGE YIELD %
+            let sumYields = 0;
+            let avYield;
+            for (let x = 0; x < divHistory.length; x++) {
+                sumYields += divHistory[x].dividend;
+            }
+            avYield = (sumYields / (price * divHistory.length)) * 100;
+
+
+            // CALCULATING AVERAGE WEIGHTED YIELD %
+            let avWeightYield;
+            let sumWeightYields = 0;
+            for (let y = 0; y < 5; y++) {
+                if (y === 0) sumWeightYields += divHistory[y].dividend * 3;
+                else if (y === 1) sumWeightYields += divHistory[y].dividend * 2;
+                else sumWeightYields += divHistory[y].dividend;
+            }
+            avWeightYield = ((sumWeightYields) / (price * 8)) * 100;
+
+
+            // DISPLAYING EACH COMPANY DATA
+            console.log(
+                "Share: " + companies[i].share +
+                "\nCompany: " + companies[i].company +
+                "\nPrice: " + price +
+                "\nLast year dividend: " + divHistory[0].dividend +
+                "\nDividend yield-%: " + yiield.toFixed(1) +
+                "\n5-year average dividend yield-%: " + avYield.toFixed(1) +
+                "\n5-year weighted average dividend yield-%: " + avWeightYield.toFixed(1) + "\n"
+            );
+
+            /*console.log("Share: " + companies[i].share);
+            console.log("Company: " + companies[i].company);
+            console.log("Price: " + price);
+            console.log("Last year dividend: " + divHistory[0].dividend);
+            console.log("Dividend yield-%: " + yiield.toFixed(1));
+            console.log("5-year average dividend yield-%: " + avYield.toFixed(1));
+            console.log("5-year weighted average dividend yield-%: " + avWeightYield.toFixed(1));*/
+        }
+    }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <button onClick={displayCompanies}>Show Companies</button>
       </header>
     </div>
   );
